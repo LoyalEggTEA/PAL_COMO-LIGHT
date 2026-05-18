be603c6d0b3e8610310f4ededda9ece63463cf5a
LoyalTEA <joyaloyal@gmail.com>
2026-05-18 01:29:19 -0700

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+set "JAVA_EXE=C:\Program Files\Java\jdk-17\bin\java.exe"
+if "%SAMBANOVA_API_KEY%"=="" (
+    echo SAMBANOVA_API_KEY is not set, so Palcom will use fallback canned replies.
+    echo Run setup_sambanova_key.bat if you want AI-powered replies.
+    echo.
+)
+"%JAVA_EXE%" -jar PalcomAssistant.jar
+pause
